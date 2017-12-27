23360e23fa9642e15dea7f2a83b1b2c252d924c4
Maegner <franciscommaguiar@gmail.com>
2017-12-27 02:07:16 +0000

    minor fix word break

--- algorithms/wordBreak.py
+++ algorithms/wordBreak.py
@@ -11,18 +11,19 @@ Return true because "leetcode" can be segmented as "leet code".
 def wordBreakAux(s,wordDict,prevSeen):
     if(s in prevSeen):
         return prevSeen[s]
-    for i in range(1,len(s),1):
+    for i in range(1,len(s)+1,1):
         prefix = s[0:i]
         sufix = s[i:]
         if prefix in wordDict and wordBreakAux(sufix,wordDict,prevSeen):
-            prevSeen[s[0:i]]=True
+            prevSeen[s]=True
             return True
     prevSeen[s] = False
     return False
     
 
 def wordBreak(s, wordDict):
+    if s == "" or wordDict == []:
+        return False
     prevSeen = {}
     prevSeen[""] = True
     return wordBreakAux(s,wordDict,prevSeen)
-    
